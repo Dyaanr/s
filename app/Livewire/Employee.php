5ed782f517adc1e168e55f4be398d4563317bf11
alexanderrotty <alexanderrotty7@gmail.com>
2025-02-26 13:51:38 +0800

    menambahkan bulk delete

--- app/Livewire/Employee.php
+++ app/Livewire/Employee.php
@@ -16,6 +16,7 @@ class Employee extends Component
     public $UpdateData = false;
     public $employee_id;
     public $katakunci;
+    public $employee_selected_id = [];
 
     public function store()
     {
@@ -73,19 +74,29 @@ class Employee extends Component
 
         $this->UpdateData = false;
         $this->employee_id = '';
+        $this->employee_selected_id = [];
     }
 
     public function delete()
     {
-        $id = $this->employee_id;
-        ModelsEmployee::find($id)->delete();
+        if($this->employee_id != ''){
+            $id = $this->employee_id;
+            ModelsEmployee::find($id)->delete();
+        }
+        if(count($this->employee_selected_id)){
+            for($x=0;$x<count($this->employee_selected_id);$x++){
+                ModelsEmployee::find($this->employee_selected_id[$x])->delete();
+            }
+        }
         session()->flash('message','Data berhasil di-delete');
         $this->clear();
     }
 
     public function delete_confirmation($id)
     {
-        $this->employee_id = $id;
+        if($id != ''){
+            $this->employee_id = $id;
+        }
     }
 
     public function render()
--- resources/views/livewire/employee.blade.php
+++ resources/views/livewire/employee.blade.php
@@ -71,10 +71,17 @@
         <div class="pb-3 pt-3">
             <input type="text" class="form-control mb-3 w-25" placeholder="Search..." wire:model.live="katakunci">
         </div>
+
+        @if ($employee_selected_id)
+        <a wire:click="delete_confirmation('')"class="btn btn-danger btn-sm mb-3"
+            data-bs-toggle="modal" data-bs-target="#exampleModal">Del {{ count($employee_selected_id) }}data</a>
+        @endif
+
         {{$dataEmployees->links()}}
         <table class="table table-striped">
             <thead>
                 <tr>
+                    <th></th>
                     <th class="col-md-1">No</th>
                     <th class="col-md-4">Nama</th>
                     <th class="col-md-3">Email</th>
@@ -86,6 +93,7 @@
                 @foreach ($dataEmployees as $key => $value)
 
                 <tr>
+                    <td><input type="checkbox" wire:key="{{ $value->id }}" value="{{ $value->id }}"wire:model.live="employee_selected_id"></td>
                     <td>{{ $dataEmployees->firstItem() + $key }}</td>
                     <td>{{ $value->nama }}</td>
                     <td>{{ $value->email }}</td>
